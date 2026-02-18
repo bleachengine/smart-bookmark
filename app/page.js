6dae3719b6a6d72b419404d3d2451ba7418b9644
bleachengine <nitish.sethi@hotmail.com>
2026-02-18 08:21:40 +0530

    Added google login and connected with supabase

--- app/page.js
+++ app/page.js
@@ -1,9 +1,55 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { supabase } from "../lib/supabase"
+
 export default function Home() {
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser()
+      console.log("bfddovdfvdomosp:",data)
+      setUser(data.user)
+    }
+
+    getUser()
+  }, [])
+
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    })
+  }
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    setUser(null)
+  }
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-semibold">
-        Smart Bookmark App
-      </h1>
+    <main className="flex min-h-screen items-center justify-center flex-col gap-4">
+
+      {!user ? (
+        <button
+          onClick={handleLogin}
+          className="px-6 py-3 bg-black text-white rounded-lg"
+        >
+          Sign in with Google
+        </button>
+      ) : (
+        <>
+          <p>Welcome, {user.email}</p>
+          <button
+            onClick={handleLogout}
+            className="px-6 py-3 bg-red-500 text-white rounded-lg"
+          >
+            Logout
+          </button>
+        </>
+      )}
+
     </main>
-  );
+  )
 }
